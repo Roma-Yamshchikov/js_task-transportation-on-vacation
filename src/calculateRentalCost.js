7dcae88355be498c24457a818772ede15421c970
Roman <yamshchikovr11@gmail.com>
2026-03-09 20:33:41 +0200

    Fixed the code and added a variable

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,9 +4,10 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
+  const costPerDAY = 40;
   const discount7Days = 50;
   const discount3Days = 20;
-  const sum = days * 40;
+  const sum = days * costPerDAY;
 
   if (days >= 7) {
     return sum - discount7Days;
